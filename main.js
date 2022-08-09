22e6fc86e5610e53c96249367d72e5b2faf98ff7
azoozalharte <zezo50115@protonmail.com>
2022-08-09 15:37:05 +0300

    clear input after adding books

--- main.js
+++ main.js
@@ -86,5 +86,8 @@ addButton.addEventListener("click", function () {
   } else {
     let newBook = new Book(title, author, pages, false);
     addBookToLibrary(newBook);
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
   }
 });
